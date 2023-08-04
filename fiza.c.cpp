1aaaafd30d197a9e9e38ee02dbfd1ca9300ccd14
fizaparween29 <141391290+fizaparween29@users.noreply.github.com>
2023-08-04 20:49:29 +0530

    Add files via upload

---
+++ fiza.c.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i;
+
+
+
+
+ for(i=0;i<10000000000000;i++)
+{
+
+
+
+
+
+ printf("I LOVE MY MOM DAD ♥️ \3");
+}
+
+
+
+
+
+ return 0;
+
+
+
+
+}
